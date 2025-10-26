b3d64792a187c4c050fb9d18c583f26aed484214
cballende <cba.allende@gmail.com>
2025-10-26 11:29:05 -0300

    Styles spit and adding components to main

--- vibraApp/src/App.tsx
+++ vibraApp/src/App.tsx
@@ -6,24 +6,30 @@ import './App.css'
 /* Dependencies  */
 import { useState } from "react";
 
+/* Context */
 import MusicProvider from './context/MusicProvider';
 
 
 /* Components */
+import Waves from './components/layouts/Canvas'
+import Header from './components/layouts/Header'
 import Main from './components/Main'
 import {Sidebar} from './components/Sidebar'
 import MusicPlayer from './components/MusicPlayer'
 
 /* types */
-import type {ReproduceProps} from "../src/types/reproduceProps";
 
 function App() {
   return (
     <>
       <MusicProvider>
-        <Sidebar />
-        <Main/>
-        <MusicPlayer/>
+        <Waves/>
+        <div className="page-container">
+          <Header/>
+          <Sidebar />
+          <Main/>
+          <MusicPlayer/>
+        </div>  
       </MusicProvider>
     </>
   )
---
+++ vibraApp/src/components/layouts/Canvas.tsx
@@ -0,0 +1,8 @@
+import "./canvas.css"
+export default function Waves() {
+ return(
+         <>
+           <canvas id="waves"></canvas>
+         </>
+        )
+}
